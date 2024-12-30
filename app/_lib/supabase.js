228a79f5ffb96ec630f773f54fb57783061a2701
Alp Kaan Akin <alpkaanakin@gmail.com>
2024-12-30 17:10:13 +0300

    fix cannot find supabaseUrl error

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-	"https://corscsvwglmbbjwihpfn.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvcnNjc3Z3Z2xtYmJqd2locGZuIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNDg3ODMxNCwiZXhwIjoyMDUwNDU0MzE0fQ.uymOXtaAOtU8Fayy74LlrZ_SaA5vzo8YH8nB3K5V_v4"
-);
+const supabasePublicUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+
+export const supabase = createClient(supabasePublicUrl, supabaseServiceKey);
